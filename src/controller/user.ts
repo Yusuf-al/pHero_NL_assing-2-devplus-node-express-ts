01c8cc1432f5cd48405d15a7244dbcb01734d5ff
Yusuf-al <naiem.n123@gmail.com>
2026-05-26 16:35:17 +0600

    password hashed

--- src/controller/user.ts
+++ src/controller/user.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from "express";
 import { pool } from "../utilities/config.ts";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response) => {
   const { name, password, role, email } = req.body;
-
+  const hashPass = await bcrypt.hash(password, 10);
+  console.log(hashPass);
   const result = await pool.query(
     `
       INSERT INTO users (name, email, password, role) 
       VALUES ($1,$2,$3,$4) RETURNING name, email, role
     `,
-    [name, email, password, role],
+    [name, email, hashPass, role],
   );
 
   res.status(200).json({
